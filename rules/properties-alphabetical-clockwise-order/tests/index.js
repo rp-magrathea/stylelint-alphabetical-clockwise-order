d24e7a243b8682ffbe69f39d960bafa47dcbe3e5
daine <veralidaine.thewildmage@gmail.com>
2020-05-20 21:19:02 -0600

    add tests

--- rules/properties-alphabetical-clockwise-order/tests/index.js
+++ rules/properties-alphabetical-clockwise-order/tests/index.js
@@ -4,6 +4,89 @@ const { ruleName, messages } = rule;
 
 testRule({
 	ruleName,
+	config: [
+		true,
+		{
+			disableFix: true,
+		},
+	],
+	fix: false,
+
+	accept: [
+		{
+			code: 'a { margin-top: 1em; margin-right: 1em; margin-bottom: 1em; margin-left: 1em; }',
+		},
+		{
+			code: 'a { border-bottom-color: teal; border-bottom-width: 1px; }',
+		},
+		{
+			code: 'a { margin-right: 1em; padding-top: 1em; }',
+		},
+	],
+
+	reject: [
+		{
+			code: 'a { margin-right: 1em; margin-top: 1em; }',
+			fixed: 'a { margin-top: 1em; margin-right: 1em; }',
+			message: messages.expected('margin-top', 'margin-right'),
+		},
+		{
+			code: 'a { border-bottom-width: 1em; border-bottom-color: teal; }',
+			fixed: 'a { border-bottom-color: teal; border-bottom-width: 1px; }',
+			message: messages.expected('border-bottom-color', 'border-bottom-width'),
+		},
+		{
+			code: 'a { padding-top: 1em; margin-right: 1em; }',
+			fixed: 'a { margin-right: 1em; padding-top: 1em; }',
+			message: messages.expected('margin-right', 'padding-top'),
+		},
+	],
+});
+
+testRule({
+	ruleName,
+	config: [
+		true,
+		{
+			disableFix: true,
+			strictAlphabetical: true,
+		},
+	],
+	fix: true,
+
+	accept: [
+		{
+			code: 'a { margin-bottom: 1em; margin-left: 1em; margin-right: 1em; margin-top: 1em; }',
+		},
+		{
+			code: 'a { border-bottom-color: teal; border-bottom-width: 1px; }',
+		},
+		{
+			code: 'a { margin-right: 1em; padding-top: 1em; }',
+		},
+	],
+
+	reject: [
+		{
+			code: 'a { margin-top: 1em; margin-right: 1em; }',
+			fixed: 'a { margin-right: 1em; margin-top: 1em; }',
+			message: messages.expected('margin-right', 'margin-top'),
+		},
+		{
+			code: 'a { border-bottom-width: 1em; border-bottom-color: teal; }',
+			fixed: 'a { border-bottom-color: teal; border-bottom-width: 1px; }',
+			message: messages.expected('border-bottom-color', 'border-bottom-width'),
+		},
+		{
+			code: 'a { padding-top: 1em; margin-right: 1em; }',
+			fixed: 'a { margin-right: 1em; padding-top: 1em; }',
+			message: messages.expected('margin-right', 'padding-top'),
+		},
+	],
+});
+
+testRule({
+	ruleName,
 	config: [true],
 	fix: true,
 
